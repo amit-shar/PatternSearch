db63d59627f92548b849e760609dc4388e5e5737
amits <amits@PRINHYDSPLN0153>
2015-04-21 18:52:30 +0530

    changes have been done in patternsearchimpl

--- PatternSearch/src/main/java/com/pramati/patternsearch/PatternFinder.java
+++ PatternSearch/src/main/java/com/pramati/patternsearch/PatternFinder.java
@@ -12,13 +12,26 @@ public class PatternFinder {
 		String firstFile= args[0];
 		String secondFile= args[1];
 		
-		System.out.println(secondFile);
 		PatternSearchService patternSearchService= new PatternSearchServiceImpl();
 		
-		patternSearchService.findMatcher(firstFile, secondFile);
 		
+		long startTime = System.currentTimeMillis();
 		
+		if(firstFile!=null && secondFile!=null)
+		{
+			patternSearchService.findMatcher(firstFile, secondFile);
+		}
 		
+		else
+		{
+			System.out.println("Please enter absolute file path");
+		}
+		
+        long endTime = System.currentTimeMillis();
+
+		System.out.println("Time taken" +(endTime - startTime));  
+			
+			
 	}
 
 }
--- PatternSearch/src/main/java/com/pramati/patternsearch/services/PatternSearchServiceImpl.java
+++ PatternSearch/src/main/java/com/pramati/patternsearch/services/PatternSearchServiceImpl.java
@@ -15,44 +15,62 @@ import java.util.List;
 
 public class PatternSearchServiceImpl implements PatternSearchService {
 
+
+
 	public void findMatcher(String firstPath, String secondPath) {
 
 		List<String> wordList= new ArrayList<String>();
-		
+
 
 		String firstFileContent=convertFileTextToString(firstPath);
 		String secondFileContent=convertFileTextToString(secondPath);
-        
-	
-		String textStr[] = firstFileContent.split("\\r?\\n");
-		
 
-		for(int i=0;i<textStr.length;i++)
-		{
-			String [] wordsInLine=textStr[i].split("\\s+");
-	    	wordList.add(wordsInLine[0].toLowerCase()+wordsInLine[wordsInLine.length-1].toLowerCase());	
+		if(firstFileContent!=null && firstFileContent!=""){
+
+			String textStr[] = firstFileContent.split("\\r?\\n");
+			String line;
+
+			for(int i=0;i<textStr.length;i++)
+			{
+				line=textStr[i];
+
+				if(line!="" || line!=" "){
+
+					String [] wordsInLine=line.split("\\s+");
+					wordList.add(wordsInLine[0].toLowerCase()+wordsInLine[wordsInLine.length-1].toLowerCase());	
+				}
+
+			}
+
+			List<String> commonString=findCommonString(secondFileContent,wordList);
+
+			display(commonString);
 
 		}
 
-		List<String> commonString=findCommonString(secondFileContent,wordList);
-
-		display(commonString);
+		else
+		{
+			System.out.println("First file is empty");
+		}
 
 	} 
 
 	private void display(List<String> commonString) {
 
-		if(commonString!=null)
+		if(commonString!=null && commonString.size()>0)
 		{
-			System.out.println(commonString.size());
+			System.out.println("Size of common string list is : "+commonString.size());
+			
 			Iterator<String> it = commonString.iterator();
 
 			while(it.hasNext())
+			{
 				System.out.println(it.next());
+			}
 		}
 
 		else
-			System.out.println("No common string exists");
+			System.out.println("No common String exists");
 
 	}
 
@@ -60,43 +78,44 @@ public class PatternSearchServiceImpl implements PatternSearchService {
 
 
 		List<String> commonString= new ArrayList<String>();
+
 		if(wordList!=null && secondFileContent!=null){
 
 			String word;
-
-
 			String textStr[] = secondFileContent.split("\\r?\\n");
-			
 
 			for(int i=0;i<textStr.length;i++)
 			{   
 				word=textStr[i];
-		    	String [] wordsInLine=textStr[i].split("\\s+");;
-		    	
-		    	if(wordList.contains(wordsInLine[0].toLowerCase()+wordsInLine[wordsInLine.length-1].toLowerCase())
-		    			|| wordList.contains(wordsInLine[0].toLowerCase())) 
-					commonString.add(word);
+
+				if(word!=" " || word!=""){
+					String [] wordsInLine=textStr[i].split("\\s+");;
+
+					if(wordList.contains(wordsInLine[0].toLowerCase()+wordsInLine[wordsInLine.length-1].toLowerCase())
+							|| wordList.contains(wordsInLine[0].toLowerCase())) 
+					{
+						commonString.add(word);
+					}
+
+				}
 
 			}
-			
 		}
 		return commonString;	
 
 	}
 
-	@SuppressWarnings("resource")
+	
 	private String convertFileTextToString(String path)
 	{
-
 		String fileContent="";
-		//InputStream is = null;
 
 		BufferedReader br=null;
 
 		try {
 			br = new BufferedReader(new FileReader(path));
-
 			Writer out = new StringWriter();
+
 			for(int i=br.read();i!=-1;i=br.read()){
 				out.write(i);
 			}
@@ -104,13 +123,13 @@ public class PatternSearchServiceImpl implements PatternSearchService {
 			fileContent=out.toString();
 
 		}catch (IOException e) {
-             e.printStackTrace();
+			System.out.println("In PatternSearchServiceImpl : convertFileTextToString(). File IO exception");
 
 		} finally {
 			try {
 				if (br != null) br.close();
 			} catch (IOException ioe) {
-
+				System.out.println("In PatternSearchServiceImpl : convertFileTextToString(). Exception occurred while closing the file");
 			}
 
 		}
